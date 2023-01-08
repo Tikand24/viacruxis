f30660b0a7d9fd9271908efa6bf14b2b3a5be0e1
jonatan_villalobos <tikand24@gmail.com>
2023-01-08 13:23:37 -0500

    styles for large devices

--- components/CardStation.jsx
+++ components/CardStation.jsx
@@ -5,49 +5,42 @@ import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
 import { TitleText, Psalms } from '../components';
 import styles from '../styles';
 
-const CardStation = ({ title, description, fadeInType, img }) => (
-  <>
+const CardStation = ({ title, description, fadeInType, img, index }) => (
+  <motion.div
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: false, amount: 0.25 }}
+    className={`${styles.innerWidth} mx-auto flex flex-col-reverse  ${fadeInType != 'left' ? 'lg:flex-row' : 'lg:flex-row-reverse'} gap-8`}
+    key={`index${index}`}
+  >
     <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+      variants={fadeIn(fadeInType, 'tween', 0.2, 1)}
+      className="flex-1 flex justify-center flex-col"
     >
-      <motion.div
-        variants={fadeIn(fadeInType, 'tween', 0.2, 1)}
-        className="flex-[0.75] flex justify-center flex-col"
-      >
-        <div className="mt-[31px] flex flex-col w-full gap-[24px]">
-          <TitleText title={<>{title}</>} />
-        </div>
-      </motion.div>
-      <motion.div
-        variants={planetVariants(fadeInType)}
-        className={`flex-1 ${styles.flexCenter}`}
-      >
-        <img src={img} className="w-[90%] h-[90%] object-contain" />
-      </motion.div>
+      <div className="mt-[31px] flex flex-col w-full gap-[24px]">
+        <TitleText title={<>{} {title}</>} />
+      </div>
+      <Psalms />
+      <div className="mt-[8px] font-normal text-secondary-white">
+        {description.map((desc) => (
+          <>
+            <p className="mt-[18px]">{desc}</p>
+          </>
+        ))}
 
-      <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)}
-        className="flex-[0.75] flex justify-center flex-col"
-      >
-        <Psalms />
-        <div className="mt-[8px] font-normal text-secondary-white">
-          {description.map((desc) => (
-            <>
-              <p className="mt-[18px]">{desc}</p>
-            </>
-          ))}
-
-          <p className="flex-1 ml-[20px] mt-[20px] font-normal text-[18px] text-white italic">
-            Padre nuestro, Avemaria y gloria
-          </p>
-        </div>
-        <div class="border-b-4 border-slate-700 w-full h-[10px] mt-2" />
-      </motion.div>
+        <p className="flex-1 ml-[20px] mt-[20px] font-normal text-[18px] text-white italic">
+          Padre nuestro, Avemaria y gloria
+        </p>
+      </div>
+      <div className="border-b-4 border-slate-700 w-full h-[10px] mt-2" />
     </motion.div>
-  </>
+    <motion.div
+      variants={planetVariants(fadeInType)}
+      className={`flex-1 ${styles.flexCenter} flex-[0.75] flex justify-center flex-col`}
+    >
+      <img src={img} className="w-[90%] h-[90%] object-contain" />
+    </motion.div>
+  </motion.div>
 );
 export default CardStation;
--- sections/Stations.jsx
+++ sections/Stations.jsx
@@ -1,14 +1,12 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
-import { TypingText, TitleText, Psalms, CardStation } from '../components';
+import { TypingText, CardStation } from '../components';
 import { stationList } from '../constants';
 import styles from '../styles';
 
 const Stations = () => (
-  <section className={`${styles.paddings} relative z-10`}>
-    <div>
+  <section className={`${styles.paddings} relative z-10 `}>
+    <div className='flex justify-center items-center content-center'>
       <TypingText title="| Rezar el viacrucis" />
     </div>
     {stationList.map((station, index) => (
@@ -17,7 +15,8 @@ const Stations = () => (
         description={station.description}
         fadeInType={index % 2 == 0 ? 'left' : 'right'}
         img={station.img}
-        key={station}
+        index={`indexCardStationStation${index}`}
+        key={`indexCardStationStation${index}`}
       />
     ))}
   </section>
